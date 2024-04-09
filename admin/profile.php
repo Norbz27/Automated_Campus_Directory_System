1ca48e61706ce2f8f5ad5e47b08fe2f0859e257d
Norbz27 <bruzonnorberto@gmail.com>
2024-04-10 00:20:59 +0800

    profile page

---
+++ admin/profile.php
@@ -0,0 +1,29 @@
+<?php include_once 'header.php'?>
+   <!--  Header End -->
+   <div class="container-fluid">
+      <div class="row justify-content-center">
+         <div class="col-md-8">
+            <!-- Profile -->
+            <div class="card">
+               <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <br>
+                            <h1><b>JOHN DIAZ</b></h1>
+                            <h6 class="mb-5">ADMIN</h6>
+                            <h6 class="mb-2"><b>User Credentials</b></h6>
+                            <span>Username: admin</span> <br>
+                            <span>Password: admin</span>
+                        </div>
+                        <div class="col-md-6">
+                            <!-- Profile Picture-->
+                            <img src="assets/images/profile/user-1.jpg" alt="" width="250" height="250" class="rounded-circle">
+                        </div>
+                    </div>
+               </div>
+            </div>
+            <!-- End Profile -->
+         </div>
+      </div>
+   </div>
+<?php include_once 'footer.php'?>
